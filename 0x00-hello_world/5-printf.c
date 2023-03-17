626a99bc42bf9b4687a4c2a911ab74c20922397b
emexgodwin23 <emexgodwin@gmail.com>
2023-03-17 00:06:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - A program that print a line using the print f function
- * Return: 0 (success)
+ * Return: 0(success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		retun (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
