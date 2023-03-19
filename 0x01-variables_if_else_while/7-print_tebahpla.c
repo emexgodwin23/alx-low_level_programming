74c24f595901322a72c3bd1229c84108f0338810
emexgodwin23 <emexgodwin@gmail.com>
2023-03-19 15:28:52 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,13 +8,11 @@
 
 int main(void)
 {
-	int i;
-	char j;
 
-	for (i = 0 ; i < 10 ; i++)
-		putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; j++)
-		putchar(j)
+	char i;
+
+	for (i = 'z' ; i <= 'a' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
