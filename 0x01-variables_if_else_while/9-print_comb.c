8faaf08fe7f1dbd550b9b472e9eae1e8e5c5205d
emexgodwin23 <emexgodwin@gmail.com>
2023-03-19 17:07:22 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - main function
  *
@@ -16,7 +15,7 @@ int main(void)
 	{
 		putchar(i + '0');
 		if (i != 9)
-		{	
+		{
 			putchar(',');
 			putchar(' ');
 		}
